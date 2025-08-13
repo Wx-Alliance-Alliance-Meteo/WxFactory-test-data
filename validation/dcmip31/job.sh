c8ecad5b7e6ee16a3b563144a80a2a6dd99e3fc0
Linden Brochu <linden.brochu@ec.gc.ca>
2025-08-13 14:41:07 +0000

    Add decent dcmip31 validation case

--- validation/dcmip31/job.sh
+++ validation/dcmip31/job.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-export SCRIPT_FOLDER=$(dirname $(realpath "${BASH_SOURCE[0]}"))
+export SCRIPT_FOLDER=$1
+echo $SCRIPT_FOLDER
 if [ ! -f "$SCRIPT_FOLDER/load-env.sh" ]; then
     echo "There is no environment to load"
     exit 2
@@ -9,4 +10,4 @@ fi
 
 cd $SCRIPT_FOLDER/../../../..
 
-mpirun -n 13824 ./WxFactory $SCRIPT_FOLDER/config.ini
+mpirun -n 7776 ./WxFactory $SCRIPT_FOLDER/config.ini
--- validation/dcmip31/qsub_job.sh
+++ validation/dcmip31/qsub_job.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 export SCRIPT_FOLDER=$(dirname $(realpath "${BASH_SOURCE[0]}"))
-qsub -lselect=173:ncpus=80:mpiprocs=80:ompthreads=1:mem=160G -lwalltime=3:0:0 $SCRIPT_FOLDER/job.sh
+qsub -lselect=100:ncpus=80:mpiprocs=80:ompthreads=1:mem=160G -lwalltime=3:0:0 -- $SCRIPT_FOLDER/job.sh $SCRIPT_FOLDER
